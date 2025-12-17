a8af56385be49ae8640292f6caceed59643fd273
Marceau Pouilloux <mpouillo@student.42lyon.fr>
2025-12-17 08:09:19 +0100

    chore: removed extra files for push to 42 vogsphere

--- tests/compile_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -z "$1" ]; then
-    SIZE="1024"
-else
-    SIZE="$1"
-fi
-
-cc -Wall -Werror -Wextra -g -Dmalloc=test_malloc -DBUFFER_SIZE=$SIZE get_next_line_bonus.c get_next_line_utils_bonus.c tests/main.c -o test.out
